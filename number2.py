f067e9a9428089605624e42e65035dd472be817f
irina <lisathe1dancer@gmail.com>
2021-12-22 14:53:20 +0300

    updated number2.py

--- number2.py
+++ number2.py
@@ -1,9 +1,9 @@
 user_seconds = int(input('Введите время в секундах:'))
 
-hour = user_seconds // 3600
+hours = user_seconds // 3600
 seconds = user_seconds % 3600
 minutes = seconds // 60
-seconds_final = seconds % 60
+seconds_final= seconds % 60
 
-print(f'''Время {hour}:{minutes}:{seconds_final}''')
-# TODO: разобраться как выводить нули 02:01:01
+print(f"{hours:02}:{minutes:02}:{seconds_final:02}")
+
