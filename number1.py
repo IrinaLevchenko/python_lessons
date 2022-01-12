564b9d4ce033202651ead01c487c8f2acdc1d01a
irina <lisathe1dancer@gmail.com>
2022-01-12 22:51:25 +0300

    Corrected all files after teacher's checkup

--- number1.py
+++ number1.py
@@ -1,9 +1,13 @@
+# Реализовать скрипт, в котором должна быть предусмотрена функция расчета заработной платы сотрудника.
+# В расчете необходимо использовать формулу: (выработка в часах * ставка в час) + премия.
+# Для выполнения расчета для конкретных значений необходимо запускать скрипт с параметрами.
+
 from sys import argv
 
 
 def salary_worker():
     try:
-        salary = (int(work_hours) * int(rate_in_hours)) + int(award)
+        salary = (int(work_hours) * int(rate_in_hours)) + float(award)
         print(f'Ваша зарплата: {salary}руб')
     except ValueError:
         print('Введите числа, а не буквы!')
--- number5.py
+++ number5.py
@@ -1,4 +1,8 @@
+# Реализовать формирование списка, используя функцию range() и возможности генератора.
+# В список должны войти четные числа от 100 до 1000 (включая границы).
+# Необходимо получить результат вычисления произведения всех элементов списка.
+# Подсказка: использовать функцию reduce().
+
 from functools import reduce
-list = [num for num in range(100, 1001) if num % 2 == 0]
-print(list)
-print(reduce(lambda num_1, num_2: num_1 * num_2, list))
+
+print(reduce(lambda num_1, num_2: num_1 * num_2, [num for num in range(100, 1001) if num % 2 == 0]))
