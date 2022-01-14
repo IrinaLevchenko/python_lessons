0e153e2bf7ad72eaa79a4fbbfa723817a2c83cc8
irina <lisathe1dancer@gmail.com>
2022-01-14 20:49:26 +0300

    Corrected all files after teacher's checkup

--- number3.py
+++ number3.py
@@ -1,19 +1,30 @@
-poor_workers = []
-summary_income = 0
-number_of_workers = 0
+# Создать текстовый файл (не программно).
+# Построчно записать фамилии сотрудников и величину их окладов (не менее 10 строк).
+# Определить, кто из сотрудников имеет оклад менее 20 тысяч, вывести фамилии этих сотрудников.
+# Выполнить подсчёт средней величины дохода сотрудников.
+# Пример файла:
+# Иванов 23543.12
+# Петров 13749.32
 
-with open('file_task_3.txt', 'r') as f:
-    for line in f:
-        if int((line.split(':'))[1]) <= 20000:
-            poor_workers.append((line.split(':'))[0])
-        summary_income += int((line.split(':'))[1])
-        number_of_workers += 1
-    average_income = summary_income / number_of_workers
+# моё решение:
+# poor_workers = []
+# summary_income = 0
+# number_of_workers = 0
+#
+# with open('file_task_3.txt', 'r', encoding='utf-8') as f:
+#     for line in f:
+#         if int((line.split(':'))[1]) <= 20000:
+#             poor_workers.append((line.split(':'))[0])
+#         summary_income += int((line.split(':'))[1])
+#         number_of_workers += 1
+#     average_income = summary_income / number_of_workers
+#
+# print(f"Доход ниже 20 000руб у сотрудников: {', '.join(poor_workers)}")
+# print(f"Средний доход сотрудника фирмы: {average_income}")
 
-print(f"Доход ниже 20 000руб у сотрудников: {', '.join(poor_workers)}")
-print(f"Средний доход сотрудника фирмы: {average_income}")
-
-
-
-
-
+# решение преподавателя:
+with open('file_task_3.txt', 'r', encoding='utf-8') as f:
+    employees_dict = {line.split()[0]: float(line.split()[1]) for line in f}
+    print(f'Average salary = {round(sum(employees_dict.values()) / len(employees_dict), 3)}\n'
+          f'Employees with salary less than 20k {[e[0] for e in employees_dict.items() if e[1] < 20000]}')
+# TODO код не работает! разобраться!
--- number4.py
+++ number4.py
@@ -1,11 +1,29 @@
-rus_num = ['Один', 'Два', 'Три', 'Четыре']
+# Создать (не программно) текстовый файл со следующим содержимым:
+# One — 1
+# Two — 2
+# Three — 3
+# Four — 4
+# Напишите программу, открывающую файл на чтение и считывающую построчно данные.
+# При этом английские числительные должны заменяться на русские.
+# Новый блок строк должен записываться в новый текстовый файл.
 
-with open('file_task_4.txt', 'r') as f:
-    numbers = [int((line[-2])) for line in f]
-    content = [rus_num[int(n) - 1] + ' - ' + str(n) for n in numbers]
-    new_file_content = '\n'.join(content)
+# моё решение:
+# rus_num = ['Один', 'Два', 'Три', 'Четыре']
+#
+# with open('file_task_4.txt', 'r', encoding='utf-8') as f:
+#     numbers = [int((line[-2])) for line in f]
+#     content = [rus_num[int(n) - 1] + ' - ' + str(n) for n in numbers]
+#     new_file_content = '\n'.join(content)
+#
+# with open('new_file_task_4.txt', 'w', encoding='utf-8') as f2:
+#     f2.write(new_file_content)
+#
+# print(f'В новый файл записана информация:\n{new_file_content}')
 
-with open('new_file_task_4.txt', 'w') as f2:
-    f2.write(new_file_content)
+# решение преподавателя первое:
+rus_dict = {"One": "Один", "Two": "Два", "Three": "Три", "Four": "Четыре" }
+with open('file_task_4.txt', 'w', encoding='utf-8') as nf:
+    with open('new_file_task_4.txt', 'r', encoding='utf-8') as mf:
+        nf.write(str([line.replace(line.split()[0], rus_dict.get(line.split()[0])) for line in mf]))
 
-print(f'В новый файл записана информация:\n{new_file_content}')
+# TODO: код не работает. доделать!
