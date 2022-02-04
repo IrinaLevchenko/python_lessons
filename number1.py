5d1007cc56224f5d4cb45e166247f34e9eca905d
irina <lisathe1dancer@gmail.com>
2022-02-04 15:48:03 +0300

    all files corrected after teacher's check-up

--- number1.py
+++ number1.py
@@ -1,20 +1,66 @@
+# Реализовать класс Matrix (матрица). Обеспечить перегрузку конструктора класса (метод __init__()),
+# который должен принимать данные (список списков) для формирования матрицы.
+# Подсказка: матрица — система некоторых математических величин, расположенных в виде прямоугольной схемы.
+# Примеры матриц: 3 на 2, 3 на 3, 2 на 4.
+# 31    32         3    5    32        3    5    8    3
+# 37    43         2    4    6         8    3    7    1
+# 51    86        -1   64   -8
+# Следующий шаг — реализовать перегрузку метода __str__() для вывода матрицы в привычном виде.
+# Далее реализовать перегрузку метода __add__() для реализации операции сложения
+# двух объектов класса Matrix (двух матриц).
+# Результатом сложения должна быть новая матрица.
+# Подсказка: сложение элементов матриц выполнять поэлементно — первый элемент первой строки первой матрицы складываем с
+# первым элементом первой строки второй матрицы и т.д.
+
+# моё решение:
+# class Matrix:
+#     def __init__(self, list_1, list_2):
+#         self.list_1 = list_1  # [[2, -3, 1], [5, 4, -2]]
+#         self.list_2 = list_2  # [[4, 2, -5], [-4, 1, 3]]
+#
+#     def __str__(self):
+#         return '\n'.join(['\t'.join([str(j) for j in i]) for i in matrix_result])
+#
+#     def __add__(self):
+#         matrix_new = [[0, 0, 0],
+#                       [0, 0, 0]]  # [[6, -1, -4], [1, 5, 1]]
+#         for i in range(len(self.list_1)):
+#             for j in range(len(self.list_2[i])):
+#                 matrix_new[i][j] = self.list_1[i][j] + self.list_2[i][j]
+#         return '\n'.join(['\t'.join([str(j) for j in i]) for i in matrix_new])
+#                        # [столбец...[...это строки....].................столбец]
+#
+#
+# my_matrix = Matrix([[2, -3, 1], [5, 4, -2]], [[4, 2, -5], [-4, 1, 3]])
+# print(my_matrix.__add__())
+
+# решение преподавателя:
+from typing import Union, List
+
+a = [[5, 3, 1, 6], [4, 4, 4, 5], [9, 0, 5, 0]]
+b = [[1, 1, 1, 2], [2, 2, 2, 2], [3, 3, 3, 1]]
+
+
 class Matrix:
-    def __init__(self, list_1, list_2):
-        self.list_1 = list_1  # [[2, -3, 1], [5, 4, -2]]
-        self.list_2 = list_2  # [[4, 2, -5], [-4, 1, 3]]
+
+    def __init__(self, lists: List[Union[list, str]]):
+        self.lists = lists
 
     def __str__(self):
-        return '\n'.join(['\t'.join([str(j) for j in i]) for i in matrix_result])
+        return '\n'.join(map(str, self.lists))
 
-    def __add__(self):
-        matrix_new = [[0, 0, 0],
-                      [0, 0, 0]]  # [[6, -1, -4], [1, 5, 1]]
-        for i in range(len(self.list_1)):
-            for j in range(len(self.list_2[i])):
-                matrix_new[i][j] = self.list_1[i][j] + self.list_2[i][j]
-        return '\n'.join(['\t'.join([str(j) for j in i]) for i in matrix_new])
-                       # [столбец...[...это строки....].................столбец]
+    def __add__(self, other):
+        c = []
+        for i in range(len(self.lists)):
+            c.append([])
+            for j in range(len(self.lists[0])):
+                c[i].append(self.lists[i][j] + other.lists[i][j])
+            return '\n'.join(map(str, c))
 
 
-my_matrix = Matrix([[2, -3, 1], [5, 4, -2]], [[4, 2, -5], [-4, 1, 3]])
-print(my_matrix.__add__())
+matrix1 = Matrix(a)
+matrix2 = Matrix(b)
+
+print(matrix1, '\n')
+print(matrix2, '\n')
+print(matrix1 + matrix2)
--- number2.py
+++ number2.py
@@ -1,3 +1,14 @@
+#  Реализовать проект расчёта суммарного расхода ткани на производство одежды.
+#  Основная сущность (класс) этого проекта — одежда, которая может иметь определённое название.
+#  К типам одежды в этом проекте относятся пальто и костюм. У этих типов одежды существуют параметры:
+#  размер (для пальто) и рост (для костюма). Это могут быть обычные числа: V и H, соответственно.
+# Для определения расхода ткани по каждому типу одежды использовать формулы:
+# для пальто (V/6.5 + 0.5), для костюма (2*H + 0.3). Проверить работу этих методов на реальных данных.
+# Реализовать общий подсчет расхода ткани. Проверить на практике полученные на этом уроке знания:
+# реализовать абстрактные классы для основных классов проекта, проверить на практике работу декоратора @property.
+
+# моё решение (не использовала абстрактные методы):
+
 class Clothes:
     def __init__(self, name):
         self.name = name
@@ -37,3 +48,37 @@ costume = Costume(185)
 print(f'Costume fabric consumption: {costume.fabric_consumption()}')
 
 print(clothes.total_fabric)
+
+# решение преподавателя:
+# from abc import ABC, abstractmethod
+#
+#
+# class Clothes(ABC):
+#     def __init__(self, param):
+#         self.param = param
+#
+#     @property
+#     @abstractmethod
+#     def consumption(self):
+#         pass
+#
+#     def __add__(self, other):
+#         return self.consumption + other.consumption
+#
+#
+# class Coat(Clothes):
+#     @property
+#     def consumption(self):
+#         return round(self.param / 6.5) + 0.5
+#
+#
+# class Costume(Clothes):
+#     @property
+#     def consumption(self):
+#         return (2 * self.param + 0.3) / 100
+#
+#
+# coat = Coat(58)
+# costume = Costume(240)
+# print(coat + costume)
+#
