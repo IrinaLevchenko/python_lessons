777a8b2d5d243efe466181d31801b30257e8c680
irina <lisathe1dancer@gmail.com>
2022-02-06 13:21:33 +0300

    all files corrected after teacher's check-up

--- number1.py
+++ number1.py
@@ -1,3 +1,10 @@
+# Реализовать класс «Дата», функция-конструктор которого должна принимать дату в виде строки формата «день-месяц-год».
+# В рамках класса реализовать два метода. Первый, с декоратором @classmethod.
+# Он должен извлекать число, месяц, год и преобразовывать их тип к типу «Число».
+# Второй, с декоратором @staticmethod, должен проводить валидацию числа, месяца и года (например, месяц — от 1 до 12).
+# Проверить работу полученной структуры на реальных данных.
+
+# моё решение:
 class Data:
     list = []
 
@@ -7,7 +14,7 @@ class Data:
     @classmethod
     def str_to_int(cls, day_month_year):
         Data.list = [int(el) for el in day_month_year.split('-')]
-        return f'Число:{Data.list[0]:02}, месяц:{Data.list[1]:02}, год:{Data.list[2]}.'
+        return f'День:{Data.list[0]:02}, месяц:{Data.list[1]:02}, год:{Data.list[2]}.'
 
     @staticmethod
     def validate_data(day_month_year):
@@ -25,3 +32,34 @@ print(Data.str_to_int('21-13-2022'))
 print(Data.validate_data('21-13-2022'))
 print(Data.validate_data('21-1k-2022'))
 print(Data.validate_data('1-1-1'))
+
+# решение преподавателя:
+# class Date:
+#     date = None
+#     day = None
+#     month = None
+#     year = None
+#
+#     def __init__(self, date):
+#         self.__class__.date = date
+#
+#     def __str__(self):
+#         return self.date
+#
+#     @classmethod
+#     def to_int(cls):
+#         date = [int(s) for s in cls.date.split('-')]
+#         cls.day = date[0]
+#         cls.month = date[1]
+#         cls.year = date[2]
+#         print(cls.day, cls.month, cls.year)
+#
+#     @staticmethod
+#     def validate(d):
+#         if d.month > 12 or d.month < 1:
+#             raise Exception("Месяц должен быть от 1 до 12")
+#
+#
+# d = Date("1-15-2022")
+# d.to_int()
+# Date.validate(d)
--- number2.py
+++ number2.py
@@ -1,16 +1,20 @@
+# Создайте собственный класс-исключение, обрабатывающий ситуацию деления на ноль.
+# Проверьте его работу на данных, вводимых пользователем.
+# При вводе нуля в качестве делителя программа должна корректно обработать эту ситуацию и не завершиться с ошибкой.
+
 class OwnError(Exception):
     def __init__(self, text):
         self.text = text
 
 
 try:
-    num_1 = int(input('Введите числитель: '))
-    num_2 = int(input('Введите знаменатель: '))
-    if num_2 == 0:
+    divident = int(input('Введите числитель: '))
+    divider = int(input('Введите знаменатель: '))
+    if divider == 0:
         raise OwnError('Делить на ноль нельзя!')
 except OwnError as err:
     print(err)
 except ValueError:
     print('Вводите только числа!')
 else:
-    print(num_1 / num_2)
+    print(f'Результат: {divident / divider}')
